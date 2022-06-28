f5bf53ae5fb9b1b23426f7513ed9bb3a18224081
samuel <samueldesta323@gmail.com>
2022-06-28 14:34:59 +0300

    removed one 'and' from the string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,8 +13,8 @@ while True:
 print(f"Last digit of {number} is {num} and", end=" ")
 
 if num > 5:
-    print("and is greater than 5")
+    print("is greater than 5")
 elif num == 0:
-    print("and is 0")
+    print("is 0")
 else:
-    print("and is less than 6 and not 0")
+    print("is less than 6 and not 0")
