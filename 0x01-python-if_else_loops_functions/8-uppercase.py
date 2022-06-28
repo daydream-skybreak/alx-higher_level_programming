225006603feb7755032d951af0f050855991c82c
samuel <samueldesta323@gmail.com>
2022-06-28 16:40:04 +0300

    added two python files

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 65 and ord(i) <= 90:
+            print("{0}".format(chr(ord(i)+32)), end = "")
+        else:
+            print("{0}".format(i, end = "")
