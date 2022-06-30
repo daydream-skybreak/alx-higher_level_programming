004fb3e08a23faaee1014518cc28bb785aba71f6
samuel <samueldesta323@gmail.com>
2022-06-30 22:01:38 +0300

    added file 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+
+    b = len(sys.argv) - 1
+    if b == 0:
+        print("{} arguments.".format(b))
+    elif b == 1:
+        print("{} argument:\n{}: {}".format(b, 1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(b))
+        for i in range(1, b + 1):
+            print("{}: {}".format(i, sys.argv[i]))
