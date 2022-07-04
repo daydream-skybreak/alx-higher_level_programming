31f4fa40e4204664f4804e709cdf8830857cde31
Samuel <samueldesta323@gmail.com>
2022-07-04 22:27:48 +0300

    added file 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,3 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             print("{}".format(j), end=" " if j != i[-1] else "")
         print()
-
-print_matrix_integer([[5,4,3],[2,1,7]]
