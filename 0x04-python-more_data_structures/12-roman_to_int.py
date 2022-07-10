4d9d4917957b292683291ee8f4322635719eacdc
Samuel <samueldesta323@gmail.com>
2022-07-10 19:59:07 +0300

    fixed unbound variable error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,6 +5,7 @@ def roman_to_int(roman_string):
                "C": 100, "CD": 400, "D": 500, "CM": 900,
                "M": 1000}
     intN = 0
+    i = 0
     while i < len(roman_string):
         if i + 1 < len(roman_string) and roman_string[i:i + 2] in numbers:
             intN += numbers[roman_string[i:i + 2]]
