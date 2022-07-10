fbafb7562061a42d53a35b690ab144f259b1a439
Samuel <samueldesta323@gmail.com>
2022-07-10 14:57:05 +0300

    fixed the function

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    keys = list(a_dictionary.keys())
-    values = list(a_dictionary.values())
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
 
-    return keys[values.index(max(values))]
+    bigK = list(a_dictionary.keys())[0]
+    bigV = a_dictionary[bigK]
+
+    for i, j in a_dictionary.items():
+        if j > bigV:
+            bigK = i
+            bigV = j
+    return bigK
