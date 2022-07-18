73f96e09cbbab6959f94e7578c11293ad837f8a2
Samuel <samueldesta323@gmail.com>
2022-07-18 23:49:54 +0300

    added TypeError exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,12 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (TypeError, ValueError):
         return False
+
+nb = safe_print_integer(57.43)
+nc = safe_print_integer("School")
+if not nb:
+    print("not printed")
+if not nc:
+    print("not printed")
