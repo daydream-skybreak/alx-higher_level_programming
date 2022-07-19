3b729be4ab11a020407cf7a160c0fdbca711a0ba
Samuel <samueldesta323@gmail.com>
2022-07-19 18:53:25 +0300

    modified to append 0 when programs in to error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,10 +6,13 @@ def list_division(my_list_1, my_list_2, list_length):
             val = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
+            val = 0
         except TypeError:
             print("wrong type")
+            val = 0
         except IndexError:
             print("out of range")
+            val = 0
         finally:
             new_list.append(val)
     return new_list
