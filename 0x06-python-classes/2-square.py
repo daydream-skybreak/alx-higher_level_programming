d4efafbdfddf5aa593df258ed7381c0c4e0b719a
Samuel <samueldesta323@gmail.com>
2022-07-20 01:06:30 +0300

    moved file 2 to the right folder

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" define a class for a square """
+
+
+class Square:
+    """ represents a square """
+    def __init__(self, size):
+        """ initializing the instance variable
+
+        Args:
+            size (int): length of the square side
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
