2e0fa4c9edd08d589a857343b9a4e2eaf2046888
Samuel <samueldesta323@gmail.com>
2022-07-20 01:27:25 +0300

    added file 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" define a class for a square """
+
+
+class Square:
+    """ represents a square """
+    def __init__(self, size=0):
+        """ initializing the instance variable
+
+        Args:
+            size (int): length of the square side
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        self.__x = value
