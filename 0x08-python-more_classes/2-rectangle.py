e3792420097a3c257f939a279717ffce20247c7f
Samuel <samueldesta323@gmail.com>
2022-07-26 00:34:57 +0300

    added file 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+""" Defines a class for creating rectangles """
+
+
+class Rectangle:
+    """ a rectangle class with private width and height"""
+    def __init__(self, width=0, height=0):
+        """ an instance method to be called when new object is created """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the rectangle """
+        self.width * self.height
+
+    def perimeter(self):
+        """ returns the perimeter of the rectangle """
+        2 * (self.width + self.height)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
