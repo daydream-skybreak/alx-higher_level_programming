445a96415dfe222057acae461a262c5241f7caf5
Samuel <samueldesta323@gmail.com>
2022-07-26 19:21:55 +0300

    task 1 done

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a file to add two numbers together"""
+
+
+from cmath import nan
+
+
+def add_integer(a, b=98):
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a + b
