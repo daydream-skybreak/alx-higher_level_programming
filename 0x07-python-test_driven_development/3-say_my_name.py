92005d5acc5a68e808074a9b2b683c7a7639edb1
Samuel <samueldesta323@gmail.com>
2022-07-29 13:20:58 +0300

    added doumentation to the funtion

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,12 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """ """
+    """
+    Function to print out the message My name is [<first_name> <last_name>]
+    Args:
+        first_name: required string argument
+        last_name: optional string argument
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
