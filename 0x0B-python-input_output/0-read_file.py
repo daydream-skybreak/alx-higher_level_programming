121fe403e7691acfc8c2c60333d9ce2f1d0688b6
Samuel <samueldesta323@gmail.com>
2022-08-03 01:46:44 +0300

    updated the function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename) as f:
-        for line in f:
-            print(line, end="")
+    """function to read a file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
