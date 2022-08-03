32eeaeda3ff9fb7e1b58926545054f085b94f2ac
Samuel <samueldesta323@gmail.com>
2022-08-04 01:05:54 +0300

    added file 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,11 +3,12 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
+    """ class for rectangle metrics instantiation"""
     def __init__(self, width, height):
+        """ class instantiation method """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-
-a = Rectangle(12, 23)
