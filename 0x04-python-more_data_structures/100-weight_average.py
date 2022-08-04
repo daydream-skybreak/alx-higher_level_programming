ef0eac865ee580c46dd6dc8a4cea327e686a0a64
Samuel <samueldesta323@gmail.com>
2022-08-04 23:28:59 +0300

    added file 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    total, denom = (0, 0)
+
+    for i in range(len(my_list)):
+        val, weight = my_list[i]
+        total += (val * weight)
+        denom += weight
+
+    return total / denom
