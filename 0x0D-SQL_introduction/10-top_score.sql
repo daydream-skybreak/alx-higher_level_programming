f29caf9c09aa959843931f084e2c00ce524c1e53
Samuel <samueldesta323@gmail.com>
2022-09-06 23:57:16 +0300

    added file 9 and 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- lists all records of 'second_table'
+-- shows score and name in this order
+-- the result is ordered by score in desc ordered
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
