1ebaf871fa486aecf4728db87c3c09401feba48b
Samuel <samueldesta323@gmail.com>
2022-09-08 00:20:51 +0300

    made privileges put to effect

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,3 +3,4 @@
 -- password should be set to 'user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH auth_socket BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
