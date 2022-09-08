49756af9d45ad85578497de9536838507a3247c6
Samuel <samueldesta323@gmail.com>
2022-09-09 00:53:09 +0300

    added the following files to the right folder

--- 0x0E-SQL_more_queries/101-avg_temperatures.sql
+++
@@ -1,5 +0,0 @@
--- script that displays the average temperature of cities --
-SELECT city, AVG(value) as avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
--- 0x0E-SQL_more_queries/103-max_state.sql
+++
@@ -1,5 +0,0 @@
--- script that will show the MAX temps ordered by State"
-SELECT state, MAX(value) as max_temp
-FROM temperatures
-GROUP BY state
-ORDER BY state ASC;
