51f89345662a85040e00bab2cb400e5dec359cb6
Samuel <samueldesta323@gmail.com>
2022-09-09 00:10:45 +0300

    improved the security

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- creates MySQL server user 'user_0d_1'
 -- user should have all the privileges
 -- password should be set to 'user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH auth_socket BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
