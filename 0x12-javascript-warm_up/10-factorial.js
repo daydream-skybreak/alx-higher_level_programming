2eecd36fed8d7e47a43204176b2d870506fe5ca6
Samuel <samueldesta323@gmail.com>
2022-10-04 01:36:01 +0300

    added file 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function factorial(x) {
+    if (x === 1 || isNaN(x)) {
+        return 1;
+    } else {
+        return x * factorial(x - 1);
+    }
+}
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
