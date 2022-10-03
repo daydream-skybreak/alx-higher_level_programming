c0ee7b7ac8bf2cfbf75621408c9a68b9d6033813
Samuel <samueldesta323@gmail.com>
2022-10-03 17:39:59 +0300

    typo error

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ while (process.argv[len] !== undefined) {
   len++;
 }
 if (len === 2) {
-  console.log('No arguments');
+  console.log('No argument');
 } else {
   for (let i = 2; i < len; i++) {
     console.log(process.argv[i]);
