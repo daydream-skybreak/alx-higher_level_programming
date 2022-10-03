5e6400f409dcf181bc733cdca74b2d5e4b1cec85
Samuel <samueldesta323@gmail.com>
2022-10-04 00:41:03 +0300

    fixed an issue

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
-if (arg === undefined || arg.isNaN === undefined) {
+const arg = parseInt(process.argv[2]);
+if (arg === undefined || isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${parseInt(arg)}`);
+  console.log(`My number: ${arg}`);
 }
