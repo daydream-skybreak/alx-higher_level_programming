a39402516ebf4a53b11921fc7767bbf809271ee5
Samuel <samueldesta323@gmail.com>
2022-10-03 17:37:43 +0300

    added file 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let len = 0;
+while (process.argv[len] !== undefined) {
+  len++;
+}
+if (len === 2) {
+  console.log('No arguments');
+} else {
+  for (let i = 2; i < len; i++) {
+    console.log(process.argv[i]);
+  }
+}
