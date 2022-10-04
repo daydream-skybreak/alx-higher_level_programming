fcce57ae7b62b8a4403fa4deeba9fe20e8baf8ce
Samuel <samueldesta323@gmail.com>
2022-10-04 23:12:38 +0300

    added file 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+        if ((w > 0) && (h > 0)) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+    print() {
+        const row = [];
+        for (let i = 0; i <= this.width; i++) {
+            row.push('');
+        }
+        for (let i = 0; i < this.height; i++) {
+            console.log(row.join('X'));
+        }
+    }
+}
+module.exports = Rectangle;
