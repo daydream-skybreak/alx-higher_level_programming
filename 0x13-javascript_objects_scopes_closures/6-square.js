8870daf5901276b5e18a082ff9afb5ccdb0ec20b
Samuel <samueldesta323@gmail.com>
2022-10-05 00:38:13 +0300

    added file 6:

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const s = require('./5-square');
+class Square extends s {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i <= this.width; i++) {
+      let s = '';
+      for (let i = 0; i < this.height; i++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+module.exports = Square;
