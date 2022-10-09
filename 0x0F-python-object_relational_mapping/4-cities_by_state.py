2bd1ba25b148ed628853b75b1f06f7412bdbf5a7
Samuel <samueldesta323@gmail.com>
2022-10-09 04:06:38 +0300

    added file 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+this module will list all 'states' starting where the name is `arg`
+from the database 'hbtn_0e_0_usa'
+protects from sql injection
+
+This script will accept three arguments
+    mysql_username: the username of the mysql server
+    mysql_password: the password for the user
+    database name: the database in which 'states' table is found
+
+    server - localhost
+    port - 3306
+"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    usr = sys.argv[1]
+    paswrd = sys.argv[2]
+    data_base = sys.argv[3]
+    datab = MySQLdb.connect("localhost", usr, paswrd, data_base, port=3306)
+    cur = datab.cursor()
+    cur.execute(" SELECT cities.id, cities.name, states.name FROM cities \
+        join states on states.id = cities.state_id\
+        ORDER BY cities.id")
+    r = cur.fetchall()
+    for row in r:
+        print(row)
+    cur.close()
+    datab.close()
