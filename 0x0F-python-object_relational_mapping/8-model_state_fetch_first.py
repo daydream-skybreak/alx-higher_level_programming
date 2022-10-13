d9d4fdca1b1ede8e67bed11fbfb52ffa76fad820
Samuel <samueldesta323@gmail.com>
2022-10-13 10:35:57 +0300

    added file 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script to list all state objects
+"""
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    s = session.query(State).order_by(State.id).first()
+    if s is not None:
+        print("{}: {}".format(s.id, s.name))
