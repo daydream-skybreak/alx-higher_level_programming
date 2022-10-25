508fde4561a667ffa84b2c50ba82804e2d7c98b8
Samuel <samueldesta323@gmail.com>
2022-10-25 23:32:48 +0300

    added file 6

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,6 @@ fetches https://alx-intranet.hbtn.io/status
 """
 if __name__ == '__main__':
     import requests
-    r = requests.get('http://0.0.0.0:5050/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     t = r.reason
     print("Body response:\n\t- type: {}\n\t- content: {}".format(type(t), t))
