ac152672baa131a1b7b302e7aed98b7435f917be
Samuel <samueldesta323@gmail.com>
2022-10-25 16:25:37 +0300

    added file 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email
+sends a POST request to the passed URL with the email as a parameter
+displays the body of the response
+"""
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    from sys import argv
+
+    url = argv[1]
+    email = {
+        'email': argv[2],
+    }
+    value = urllib.parse.urlencode(email).encode('ascii')
+    request = urllib.request.Request(url, value)
+
+    with urllib.request.urlopen(request) as res:
+        print(res.read().decode('utf-8'))
