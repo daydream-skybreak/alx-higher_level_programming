dde47a767f3614b0348b4f95904081b1e1bf313d
Samuel <samueldesta323@gmail.com>
2022-11-17 15:09:25 +0300

    added task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url).on('response', () => {
-    console.log(`code: ${response.statusCode}`);
+request.get(url).on('response', (response) => {
+  console.log(`code: ${response.statusCode}`);
 });
