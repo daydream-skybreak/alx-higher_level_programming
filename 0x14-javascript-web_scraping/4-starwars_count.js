cb56422b60b0c4d10ad14cd072b434e073d3181c
Samuel <samueldesta323@gmail.com>
2022-11-17 22:09:40 +0300

    added task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (!error) {
+    const movies = JSON.parse(body).results;
+    console.log(movies.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
